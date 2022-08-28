36385c667eb43362903fd4279c8669b26edc7de3
Harison Lacey <63789277+HarisonLacey@users.noreply.github.com>
2022-08-28 14:26:39 +0200

    feature: init basic screen setup

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 
+// screens
+import { TeamScreen } from '../screens/Landing'
+import { PlayerScreen } from '../screens/Player'
+
 // stack param list
 type RootStackParamList = {
-    Landing: undefined
     Team: undefined
+    Player: undefined
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
@@ -14,13 +18,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 export const AppNavigator = () => (
     <NavigationContainer>
         <Stack.Navigator
+            initialRouteName="Team"
             screenOptions={{
                 animationTypeForReplace: 'push',
                 animation: 'slide_from_right',
             }}
         >
-            <Stack.Screen name="Landing" component={} options={{}} />
-            <Stack.Screen name="Team" component={} options={{}} />
+            <Stack.Screen name="Team" component={TeamScreen} options={{}} />
+            <Stack.Screen name="Player" component={PlayerScreen} options={{}} />
         </Stack.Navigator>
     </NavigationContainer>
 )
