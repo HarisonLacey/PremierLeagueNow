fb664e9db968651a35dcc26c8a138cc3c7c62a94
Harison Lacey <63789277+HarisonLacey@users.noreply.github.com>
2022-08-29 16:32:45 +0200

    chore: navigation component cleanup

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -4,12 +4,10 @@ import { NavigationContainer } from '@react-navigation/native'
 
 // screens
 import { TeamScreen } from '../screens/Team'
-import { PlayerScreen } from '../screens/Player'
 
 // stack param list
 type RootStackParamList = {
     Team: undefined
-    Player: undefined
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
@@ -17,15 +15,8 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 // app navigator component
 export const AppNavigator = () => (
     <NavigationContainer>
-        <Stack.Navigator
-            initialRouteName="Team"
-            screenOptions={{
-                animationTypeForReplace: 'push',
-                animation: 'slide_from_right',
-            }}
-        >
+        <Stack.Navigator initialRouteName="Team">
             <Stack.Screen name="Team" component={TeamScreen} options={{}} />
-            <Stack.Screen name="Player" component={PlayerScreen} options={{}} />
         </Stack.Navigator>
     </NavigationContainer>
 )
