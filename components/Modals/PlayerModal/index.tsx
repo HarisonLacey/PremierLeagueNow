c2bd0d4252e9e302539baf6908009cae1658ad99
Harison Lacey <63789277+HarisonLacey@users.noreply.github.com>
2022-08-31 10:23:05 +0200

    feature: player modal updates

--- components/Modals/PlayerModal/index.tsx
+++ components/Modals/PlayerModal/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Modal, TouchableOpacity } from 'react-native'
+import { View, Text, Modal, TouchableOpacity, StyleSheet } from 'react-native'
 
 import { PlayerModalProps } from './models'
 
@@ -9,13 +9,29 @@ export const PlayerModal = ({
     ...props
 }: PlayerModalProps) => {
     return (
-        <Modal {...props}>
-            <View>
-                <Text>Hello</Text>
-                <TouchableOpacity onPress={onRequestClose}>
-                    <Text>Close</Text>
-                </TouchableOpacity>
+        <Modal {...props} transparent>
+            <View style={styles.modalContainer}>
+                <View style={styles.modalContentContainer}>
+                    <Text>Hello</Text>
+                    <TouchableOpacity onPress={onRequestClose}>
+                        <Text>Close</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </Modal>
     )
 }
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        padding: 50,
+        flex: 1,
+        justifyContent: 'center',
+    },
+    modalContentContainer: {
+        backgroundColor: '#fff',
+        flex: 0.5,
+        borderRadius: 10,
+    },
+})
